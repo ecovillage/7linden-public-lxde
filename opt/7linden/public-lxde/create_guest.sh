4eaeef2a8a69e7368686424effafffff9f515e3d
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-09-12 07:18:43 +0200

    include chromium-autostart in guest user creation script.

--- opt/7linden/public-lxde/create_guest.sh
+++ opt/7linden/public-lxde/create_guest.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# still is interactive!
 adduser guest-prefs --gecos "Guest User Template"
 echo "guest-prefs:gup" | chpasswd
 
 mkdir /etc/guest-session
 ln -s /home/guest-prefs /etc/guest-session/skel
+
+
+mkdir -p /home/guest-prefs/.config/autostart
+cat << EOF > /home/guest-prefs/.config/autostart/autostart.desktop
+[Desktop Entry]
+Type=Application
+Exec=chromium-browser
+EOF
